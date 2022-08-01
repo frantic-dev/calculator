56c10ad1cda13f5dd0e6f910bb810c6ec77e95c7
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2022-08-01 20:21:56 +0100

    calculate long operations

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function divide(a , b) {
 function operate (operator, a , b) {
     if (operator == '+') return add(a , b);
     else if(operator == '-') return subtract(a , b);
-    else if(operator == '*') return multiply(a , b);
-    else if(operator == '/') return divide(a , b);
+    else if(operator == 'x') return multiply(a , b);
+    else if(operator == '÷') return divide(a , b);
     else return `error operator is: ${operator}, a: ${a}, b: ${b}`;
 }
 const numbers = document.querySelectorAll('.number');
@@ -23,10 +23,13 @@ let arrayNumbers = [...numbers];
 const result = document.querySelector('#result');
 let equation = document.querySelector("#print")
 
-let outputNumber = 0;       //clicking on number displays it on result div
+let outputNumber = "";       //clicking on number displays it on result div
 for(let i = 0; i <= 9; i++) {
     let number = arrayNumbers[i].textContent;
     arrayNumbers[i].addEventListener('click',() => {
+        // if(outputNumber != "") {
+        //     equation.textContent = outputNumber;
+        // }
         result.textContent += number;
         return outputNumber = result.textContent;
     })  
@@ -34,14 +37,24 @@ for(let i = 0; i <= 9; i++) {
 
 const calc = document.querySelectorAll('.operator');
 let operators = [...calc];
-
+let calcOperator;
+let firstNumber;
+let newEquation;
+let array
         //clicking on operator returns the outputNumber to equation div with the operator ,then result clears 
 for(let i = 0; i < operators.length ; i++ ) {
-    operators[i].addEventListener('click', ()=>{
-        let calcOperator = operators[i].textContent; 
-        let firstNumber = outputNumber;
+    operators[i].addEventListener('click', ()=>{ 
+        calcOperator = operators[i].textContent; 
+        firstNumber = outputNumber;
         equation.textContent += ` ${firstNumber} ${calcOperator}`;
         result.textContent = "";
+        newEquation = equation.textContent;
+        array = [...newEquation.split(' ')]
+        console.log(array)
+        if(array.length == 5) {
+            equation.textContent = " " + operate(array[2], array[1], array[3]) + " " + array[4];
+        }
+
     })
 }
             //resets
@@ -53,8 +66,20 @@ clear.addEventListener('click' , () => {
     result.textContent = "";
 } )
 
+let fullEquation;
+const equal = document.querySelector('#equal');
 
-    
-console.log(operators)
+equal.addEventListener('click', () =>{
+    equation.textContent += " " + outputNumber;
+    firstNumber = [...equation.textContent.split(' ')]
+    console.log(firstNumber)
+    result.textContent = operate(calcOperator, firstNumber[1], outputNumber);
+    fullEquation = equation.textContent;
+})
+
+
+
+
+
 
 
